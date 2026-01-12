981d4d76178cb63b85b169801841d5d3970cd1e3
DevBrandonArellano <brandon.arellano.dev@gmail.com>
2026-01-12 17:08:16 -0500

    Se realizan cambio para docker en producción, se mantiene un mismo problema con el ODBC driver 18

--- TexCore/settings.py
+++ TexCore/settings.py
@@ -153,6 +153,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
 ]
 
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
